8e5dd153bca09ab144dbe7831dbea69865b36596
jim3692 <jim3692@gmail.com>
2021-10-28 02:07:44 +0300

    fix root name

--- reload.sh
+++ reload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export root="$( cd -- "$(dirname "$0")" > /dev/null 2>&1 ; pwd -P )"
+export PROJECT_ROOT="$( cd -- "$(dirname "$0")" > /dev/null 2>&1 ; pwd -P )"
 source $PROJECT_ROOT/scripts/lib.sh
 
 bash -c "cd $PROJECT_ROOT/nginx ; docker-compose exec nginx nginx -s reload"
